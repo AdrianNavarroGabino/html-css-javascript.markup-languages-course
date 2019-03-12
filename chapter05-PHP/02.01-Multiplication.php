fb6670c0a6540e7dc6caaadafb60f0a720048ee4
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-03-12 10:54:08 +0100

    Exercise 2.1 - Multiplication

---
+++ chapter05-PHP/02.01-Multiplication.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercise 2.1</title>
+</head>
+<body>
+    <?php
+        echo "125 x 237 = ", 125*237;
+    ?>
+</body>
+</html>
