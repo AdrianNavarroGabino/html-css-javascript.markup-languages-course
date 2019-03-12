341ea46d26ada8d3e835219da425ba4f2a34c2c1
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-03-12 11:28:37 +0100

    Exercise 2.3 - Operation

---
+++ chapter05-PHP/exercise02.03-Operation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercise 2.3</title>
+</head>
+<body>
+    <?php
+        $operation = ((3 + 7) * 12) / 5;
+
+        echo "((3 + 7) * 12) / 5 = $operation";
+    ?>
+</body>
+</html>
