adc431adac209b17dafadf4ed84d83b6726fe5fa
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-03-12 11:23:36 +0100

    Example 05 - Single and double quotation marks

---
+++ chapter05-PHP/example05-php05.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+	<?php
+		// php05.php - Comillas simples y dobles
+
+		$numero = 201;
+		
+		echo "Con comillas dobles: $numero <br />";
+		echo 'Con comillas dobles: $numero <br />';
+	?>
+</body>
+</html>
