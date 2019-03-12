9e1c500b780783bbf12792f9dfa8f2bca176beec
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-03-12 11:21:56 +0100

    Exercise 2.2 - Module

---
+++ chapter05-PHP/exercise02.02-Module.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercise 2.2</title>
+</head>
+<body>
+    <?php
+        $operation = 1380 % 7;
+        
+        echo "1380 % 7 = ", $operation;
+    ?>
+</body>
+</html>
