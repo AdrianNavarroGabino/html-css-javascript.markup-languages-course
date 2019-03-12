d2cf4fe6b1066d8b703986fa338a245525f1c22f
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-03-12 11:04:06 +0100

    Variables

---
+++ chapter05-PHP/php03.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+    <?php
+        // php03.php - Variables
+        $nombre = "Adrian";
+        $numero = 5;
+        $segundoNumero = 6.7;
+        $negritaSI = "<b>";
+        $negritaNO = "</b>";
+        echo "Hola, ", $negritaSI, $nombre, $negritaNO,
+        "<br/>El primer numero es ", $numero,
+        " y el segundo es ", $segundoNumero;
+    ?>
+</body>
+</html>
