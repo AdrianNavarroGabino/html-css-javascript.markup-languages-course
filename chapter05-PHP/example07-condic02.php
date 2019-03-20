d92e9e86f7aa95f8b73b5264c8f6e61ed9893ea0
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-03-20 10:56:38 +0100

    Example 07 - Conditions with if, 2

---
+++ chapter05-PHP/example07-condic02.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+    <?php
+        // condic02.php - Condiciones con "if", 2
+        $numero = 3;
+        if ($numero > 2) {
+            echo "El número es mayor que 2";
+        }
+    ?>
+</body>
+</html>
