41530fb0df84aafe16a37f6a63845ae02354a8ec
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-08-31 12:25:30 +0200

    Information storage

---
+++ chapter07-JSON/02-informationStorage/ejercicioLibros.php
@@ -0,0 +1,45 @@
+<?php
+
+	// Tipo de flujo
+	header("Content-Type: application/json");
+	
+    // Definición de la consulta SQL
+	$consulta_sql = "select * from libros;";
+
+	// Paramètres SGBD MySQL
+	$servidor_mysql = "localhost";
+	$usuario_mysql = "adrian";
+	$password_mysql = "1234";
+	$bd_mysql = "biblioteca";
+
+	if ( ( $connexion_mysql = mysqli_connect( $servidor_mysql, $usuario_mysql, $password_mysql, $bd_mysql ) )	=== FALSE )
+	{
+		echo "";
+	}
+   	else
+	{
+		$resultado = mysqli_query( $connexion_mysql, $consulta_sql);
+       	if (mysqli_num_rows($resultado) <1)
+		{
+			echo "";
+		}
+		else
+		{					
+			while ($registro = mysqli_fetch_array($resultado, MYSQLI_ASSOC))
+			{
+				//echo "pasa 2:",$registro;
+                    $lineas[] = $registro;
+			}
+
+			// Codificación en formato JSON 
+				$datosJSON = json_encode($lineas);				
+				
+			// Envío del resultado al cliente
+			echo $datosJSON;	
+		}
+
+	}
+	
+	mysqli_close($connexion_mysql);
+
+?>
